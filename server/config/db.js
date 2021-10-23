e9f0fb147acb3ab7bb4f5464892960082640cfdb
Shahrzad Kolahdooz <shahrzad.kolahdooz@gmail.com>
2021-10-22 20:42:09 -0400

    Updated db URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports =
 {
-    "URI":"mongodb://localhost/book_store"
+    //"URI":"mongodb://localhost/book_store",
+    "URI": "mongodb+srv://Shahrzad:OSpM0oLViLQqdip9@mongodbserver.pnxp1.mongodb.net/book_store?retryWrites=true&w=majority"
 }
